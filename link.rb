0a3c0bdd2648d877e7baff39e3d740ae4e90dbdb
NekoTone <nekotone@gmail.com>
2014-02-24 16:30:59 +0100

    update for ignore git directory

--- link.rb
+++ link.rb
@@ -7,7 +7,7 @@ Dir.foreach(baseddir) do |f|
   # Affiche 
   # les fichiers en excluant ce script et tous les fichier .swp
   # les dossiers en excluant . et ..  
-  if File.file?(f) and !(f == 'link.rb' || f =~ /^*.swp/ ) or File.directory?(f) and !(f =='.' || f == '..')
+  if File.file?(f) and !(f == 'link.rb' || f =~ /^*.swp/ ) or File.directory?(f) and !(f =='.' || f == '..'|| f == '.git')
     #puts f 
     exec( "ln -s "+f+" ~/"+f)
     puts "Symbolic link "+f+" create."
