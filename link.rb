6fec6956abd858d1bc3bb6e0adbfb78301618dd0
NekoTone <nekotone@gmail.com>
2014-02-24 16:14:40 +0100

    dotfile initial
    ruby symbolic link script

---
+++ link.rb
@@ -0,0 +1,15 @@
+# Defini le full patch du repertoire courant
+baseddir = Dir.pwd
+
+# Parcours du répertoire
+Dir.foreach(baseddir) do |f| 
+
+  # Affiche 
+  # les fichiers en excluant ce script et tous les fichier .swp
+  # les dossiers en excluant . et ..  
+  if File.file?(f) and !(f == 'link.rb' || f =~ /^*.swp/ ) or File.directory?(f) and !(f =='.' || f == '..')
+    #puts f 
+    exec( "ln -s "+f+" ~/"+f)
+    puts "Symbolic link "+f+" create."
+  end
+end
