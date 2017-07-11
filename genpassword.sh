95b7ace83e78829f8fc31ed149a0a6068f91a5b5
Neko Tone <nbucco@MacBook-Pro-de-Nicolas.local>
2017-07-11 13:18:53 +0200

    add osx support

--- genpassword.sh
+++ genpassword.sh
@@ -3,5 +3,10 @@
   if [ "$l" = "" ]; then
     l=8
   fi
-  tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs 
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  LC_CTYPE=C tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs 
+fi
+
 
